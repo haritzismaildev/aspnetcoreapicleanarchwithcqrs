5bfcc8c966d4d6738b75518f69fe7513ed3342f3
haritzismaildev <haritz.ismail.dev@gmail.com>
2024-02-12 19:08:34 +0700

    updated_sc_12022024

---
+++ CwkSocial/CwkSocial.Api/Controllers/PostsController.cs
@@ -0,0 +1,19 @@
+﻿using Cwk.Domain.Modles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PostsController : Controller
+    {
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id, Text= "Hallo, Dunia!!!"};
+            return Ok(post);
+        }
+    }
+}
---
+++ CwkSocial/CwkSocial.Api/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    config.AssumeDefaultVersionWhenUnspecified = true;
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
