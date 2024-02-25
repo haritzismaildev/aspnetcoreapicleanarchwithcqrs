96783d81282a123da35c478c5408395877fff861
haritzismaildev <haritz.ismail.dev@gmail.com>
2024-02-25 18:11:48 +0700

    updated_25022024_lasts

---
+++ CwkSocial/CwkSocial.Api/Controllers/V1/PostsController.cs
@@ -0,0 +1,19 @@
+﻿using Cwk.Domain.Modles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id, Text = "Hallo, Dari Controller 1!!!" };
+            return Ok(post);
+        }
+    }
+}
--- CwkSocial/CwkSocial.Api/Program.cs
+++ CwkSocial/CwkSocial.Api/Program.cs
@@ -1,4 +1,7 @@
+using CwkSocial.Api.Options;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +13,23 @@ builder.Services.AddApiVersioning(config =>
 {
     config.DefaultApiVersion = new ApiVersion(1, 0);
     config.AssumeDefaultVersionWhenUnspecified = true;
+    config.ReportApiVersions = true;
+    config.ApiVersionReader = new UrlSegmentApiVersionReader();
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddVersionedApiExplorer(config =>
+{
+    config.GroupNameFormat = "'v'VVV";
+    config.SubstituteApiVersionInUrl = true;
+});
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
@@ -22,7 +37,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+        foreach(var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                description.ApiVersion.ToString());
+        }
+    });
 }
 
 app.UseHttpsRedirection();
