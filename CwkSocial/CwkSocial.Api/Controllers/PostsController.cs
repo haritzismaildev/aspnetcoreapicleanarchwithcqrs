9a6231c10a285c4f381869cbf35e3279ce0db7ef
haritzismaildev <haritz.ismail.dev@gmail.com>
2024-02-25 10:18:06 +0700

    Revert "updated_25022024"
    This reverts commit b9cf039cfa6914a0ef026d2fd5082ddb7374a399.

--- CwkSocial/CwkSocial.Api/Controllers/PostsController.cs
+++ CwkSocial/CwkSocial.Api/Controllers/PostsController.cs
@@ -1,18 +1,18 @@
 ﻿using Cwk.Domain.Modles;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CwkSocial.Api.Controllers.V2
+namespace CwkSocial.Api.Controllers
 {
-    [ApiVersion("2.0")]
-    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
+    [Route("api/v1/[controller]")]
     public class PostsController : Controller
     {
+
         [HttpGet]
         [Route("{id}")]
         public IActionResult GetById(int id)
         {
-            var post = new Post { Id = id, Text = "Hallo, Dari Controller V{version:apiVersion}!!!" };
+            var post = new Post { Id = id, Text= "Hallo, Dunia!!!"};
             return Ok(post);
         }
     }
